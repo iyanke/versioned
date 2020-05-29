5411a4b2b75a1f345f6b73798cc3614c065ee7c5
inna_yan <inna_yan@TeamCity>
2020-05-29 12:33:47 +0300

    TeamCity change in 'Versioned6' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,50 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.1"
+
+project {
+
+    buildType(Versioned6testConfig)
+
+    params {
+        password("system.pass88", "credentialsJSON:dbd4f01c-6f7b-4713-9395-99ad5870d0bf")
+        password("pass234", "credentialsJSON:032a32b6-8e95-49a7-bd2e-99a5f5c025b3")
+    }
+}
+
+object Versioned6testConfig : BuildType({
+    name = "versioned6_test config"
+
+    params {
+        param("param1", "test")
+    }
+
+    steps {
+        script {
+            scriptContent = """echo "Text: 12345678""""
+        }
+    }
+})
